fad26210b328b67646e058c3b7a4a014b793b1f3
jaylee <jayginwoolee98@gmail.com>
2024-01-05 17:07:17 +0900

    first commit

---
+++ MiniProject/src/com/ExpressDevs/Project/payment/paymentDTO.java
@@ -0,0 +1,25 @@
+package com.ExpressDevs.Project.payment;
+
+import java.util.Scanner;
+
+public class paymentDTO {
+
+    Scanner sc = new Scanner(System.in);
+    public void paidWithCard() {
+        System.out.println("=============== 카드 결제를 선택하셨습니다. ===============");
+        System.out.println("1. 삼성 카드");
+        System.out.println("2. 국민 카드");
+        System.out.println("3. 농협 카드");
+        System.out.println("4. 신한 카드");
+        System.out.println();
+
+
+//        switch()
+    }
+
+    public void paidWithCash() {
+        System.out.println("=============== 현금 결제를 선택하셨습니다. ===============");
+
+    }
+
+}
