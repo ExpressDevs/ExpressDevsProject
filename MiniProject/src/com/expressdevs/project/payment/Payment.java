e8922c4efaac92a4dd55982f8f95180237c4c17a
jaylee <jayginwoolee98@gmail.com>
2024-01-06 12:34:55 +0900

    first commit

--- MiniProject/src/com/expressdevs/project/payment/Payment.java
+++ MiniProject/src/com/expressdevs/project/payment/Payment.java
@@ -30,7 +30,12 @@ public class Payment {
 
     public void paidWithCash() {
         System.out.println("=============== 현금 결제를 선택하셨습니다. ===============");
-        System.out.println("1. ");
+        System.out.println("1. 결제 금액에 맞게 지불하기");
+        System.out.println("2. 만원");
+        System.out.println("3. 오천원");
+        System.out.println("4. 천원");
+        System.out.println("5. 기타");
+        System.out.println("6. ");
 
 
     }
