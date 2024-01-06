5fc36a7a476b3ced5b4d43ac7052a49dbcdd093e
antmrhdqn <antmrhdqn@gmail.com>
2024-01-06 17:56:17 +0900

    Member

--- MiniProject/src/com/expressdevs/project/run/Application.java
+++ MiniProject/src/com/expressdevs/project/run/Application.java
@@ -1,9 +1,14 @@
 package com.expressdevs.project.run;
 
+import com.expressdevs.project.model.DTO.MemberDTO;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hihi");
-        System.out.println("안된다고?");
+
+        MemberDTO DH = new MemberDTO("김동환", 26 , "ehdghks", "ehdghks123", 50000);
+        MemberDTO JW = new MemberDTO("이진우", 26 , "wlsdn", "wlsdn123", 50000);
+        MemberDTO SR = new MemberDTO("이서린", 21 , "tjfls", "tjfls123", 50000);
+
     }
 }
