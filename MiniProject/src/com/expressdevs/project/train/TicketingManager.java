90b6bc48904339a0f39dcb575e3417d1577f3e2e
JayLee-98 <jayginwoolee98@gmail.com>
2024-01-08 18:15:08 +0900

    third commit of Jan 08

--- MiniProject/src/com/expressdevs/project/train/TicketingManager.java
+++ MiniProject/src/com/expressdevs/project/train/TicketingManager.java
@@ -1,5 +1,14 @@
 package com.expressdevs.project.train;
 
-public class TicketingManager {
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketingManager {
+    List<TrainDTO> timeSchedule = new ArrayList<>();
+
+    timeSchedule.add(new TrainDTO());
 }
+
---
+++ MiniProject/src/com/expressdevs/project/train/TrainDTO.java
@@ -0,0 +1,50 @@
+package com.expressdevs.project.train;
+
+public class TrainDTO {
+    private String departure;
+    private String arrival;
+    private int price;
+
+    public TrainDTO() {
+
+    }
+
+    public TrainDTO(String departure, String arrival, int price) {
+        this.departure = departure;
+        this.arrival = arrival;
+        this.price = price;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "TrainDTO{" +
+                "departure='" + departure + '\'' +
+                ", arrival='" + arrival + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
