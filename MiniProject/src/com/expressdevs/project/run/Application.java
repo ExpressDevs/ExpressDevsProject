82406b502c245bdf1cf9d941decd9f1f3ace11c0
antmrhdqn <antmrhdqn@gmail.com>
2024-01-08 18:11:45 +0900

    train member

--- MiniProject/src/com/expressdevs/project/run/Application.java
+++ MiniProject/src/com/expressdevs/project/run/Application.java
@@ -1,15 +1,19 @@
 package com.expressdevs.project.run;
 
 import com.expressdevs.project.model.DTO.MemberDTO;
+import com.expressdevs.project.view.TicketingMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
+    public static List<MemberDTO> memberList = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        MemberDTO DH = new MemberDTO("김동환", 26 , "ehdghks", "ehdghks123", 50000);
-        MemberDTO JW = new MemberDTO("이진우", 26 , "wlsdn", "wlsdn123", 50000);
-        MemberDTO SR = new MemberDTO("이서린", 21 , "tjfls", "tjfls123", 50000);
+        new TicketingMenu().mainMenu();
 
     }
-}
 
+}
