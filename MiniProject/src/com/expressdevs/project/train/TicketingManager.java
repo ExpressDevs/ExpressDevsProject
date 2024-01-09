8abe0777460b0c4a971a5b63784f5d52bc133094
JayLee-98 <jayginwoolee98@gmail.com>
2024-01-09 18:13:31 +0900

    sixth commit of Jan 08

--- MiniProject/src/com/expressdevs/project/train/TicketingManager.java
+++ MiniProject/src/com/expressdevs/project/train/TicketingManager.java
@@ -19,15 +19,30 @@ public class TicketingManager {
     public TicketDTO startTicketing() {
         String startStation = selectStartStation();
         String endStation = selectEndStation(startStation);
-        String time = selectTime();
-        ArrayList<Integer> seatInfo = generateSeatInfo();
-
         TicketDTO td = new TicketDTO();
         td = TicketCount();
+        ArrayList<Integer> seatInfo;
+        String time;
+        String selectedTime = "";
+        while (true) {
+            time = selectTime(selectedTime);
+
+            seatInfo = generateSeatInfo();
+
+            if (seatInfo.get(2) < td.getTotal()) {
+                System.out.println("선택하신 시간대의 남은 좌석 수 : " + seatInfo.get(2));
+                System.out.println("예매 가능한 좌석의 수가 부족합니다. 다른 시간대를 선택해주세요.");
+                selectedTime = time;
+            } else {
+                break;
+            }
+
+        }
         int remainingSeats = seatInfo.get(2);
         System.out.println(time + "에 " + startStation + "에서 " + endStation + "로 가는 기차의 남은 좌석 수는 " + remainingSeats + "개입니다.");
+        System.out.println(td.getTotal() + "개 좌석 예매 가능합니다.");
 
-        reserveSeat(remainingSeats, seatInfo);
+//        reserveSeat(remainingSeats, seatInfo);
 
         return td;
 
@@ -83,10 +98,11 @@ public class TicketingManager {
         String[] stations = {"수서역", "부산역", "동대구역", "대전역", "동탄역"};
         List<String> remainingStations = new ArrayList<>(Arrays.asList(stations));
 
+
         remainingStations.remove(startStation);
 
         while (true) {
-            System.out.println("=============== 선택 가능한 도착역 ==============");
+            System.out.println("======  ========= 선택 가능한 도착역 ==============");
             for (int i = 0; i < remainingStations.size(); i++) {
                 System.out.println((i + 1) + ". " + remainingStations.get(i));
             }
@@ -103,13 +119,33 @@ public class TicketingManager {
         }
     }
 
-    public String selectTime() {
+    public String selectTime(String selectedTime) {
         Scanner sc = new Scanner(System.in);
         String time = "";
 
         while (true) {
+            int num = 1;
             System.out.println("============= 현재 예매 가능한 시간 ============");
-            System.out.println("1. 8:30\n2. 11:30\n3. 14:30\n4. 17:30\n5. 20:30");
+            if (!"8:30".equals(selectedTime)) {
+                System.out.println(num + ". 8:30");
+                num++;
+            }
+            if (!"11:30".equals(selectedTime)) {
+                System.out.println(num + ". 11:30");
+                num++;
+            }
+            if (!"14:30".equals(selectedTime)) {
+                System.out.println(num + ". 14:30");
+                num++;
+            }
+            if (!"17:30".equals(selectedTime)) {
+                System.out.println(num + ". 17:30");
+                num++;
+            }
+            if (!"20:30".equals(selectedTime)) {
+                System.out.println(num + ". 20:30");
+                num++;
+            }
             System.out.println("==============================================");
             System.out.print("시간을 입력하세요 (번호 입력) : ");
             int inputTimeNo = sc.nextInt();
@@ -142,7 +178,7 @@ public class TicketingManager {
     public ArrayList<Integer> generateSeatInfo() {
         Random random = new Random();
 
-        int totalSeats = random.nextInt(100);
+        int totalSeats = (int) (Math.random() * 100) + 10;
         int reservedSeats = random.nextInt(totalSeats);
         int remainingSeats = totalSeats - reservedSeats;
 
@@ -156,25 +192,25 @@ public class TicketingManager {
         return seatInfo;
     }
 
-    public void reserveSeat(int remainingSeats, ArrayList<Integer> seatInfo) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("==============================================");
-            System.out.print("예약하실 좌석 번호를 입력하세요 (1-" + remainingSeats + ") : ");
-            int seatNumber = sc.nextInt();
-            sc.nextLine();
-
-            if (seatNumber > 0 && seatNumber <= remainingSeats) {
-                remainingSeats--;
-                seatInfo.set(2, remainingSeats);
-                System.out.println("좌석 " + seatNumber + "번을 선택하셨습니다.");
-                break;
-            } else {
-                System.out.println("==============================================");
-                System.out.println("유효하지 않은 좌석 번호입니다. 다시 시도해주세요.");
-            }
-        }
-    }
+//    public void reserveSeat(int remainingSeats, ArrayList<Integer> seatInfo) {
+//        Scanner sc = new Scanner(System.in);
+//        while (true) {
+//            System.out.println("==============================================");
+//            System.out.print("예약하실 좌석 번호를 입력하세요 (1-" + remainingSeats + ") : ");
+//            int seatNumber = sc.nextInt();
+//            sc.nextLine();
+//
+//            if (seatNumber > 0 && seatNumber <= remainingSeats) {
+//                remainingSeats--;
+//                seatInfo.set(2, remainingSeats);
+//                System.out.println("좌석 " + seatNumber + "번을 선택하셨습니다.");
+//                break;
+//            } else {
+//                System.out.println("==============================================");
+//                System.out.println("유효하지 않은 좌석 번호입니다. 다시 시도해주세요.");
+//            }
+//        }
+//    }
 
     public TicketDTO TicketCount() {
         TicketDTO td = new TicketDTO();
@@ -190,11 +226,12 @@ public class TicketingManager {
             sc.nextLine();
 
             int count = 0;
-            if (age>0 && age < 5) {
+            if (age > 0 && age < 5) {
                 System.out.print("구매하실 티켓 매수를 입력해주세요 : ");
                 count = sc.nextInt();
                 sc.nextLine();
             } else if (age == 5) {
+                System.out.println("총 " + td.getTotal() + "매 티켓을 선택하셨습니다.");
                 return td;
             }
 
@@ -242,10 +279,10 @@ public class TicketingManager {
 
         for (TrainDTO price : timeSchedule) {
             if (price.getDeparture().equals(startStation) && price.getArrival().equals(endStation)) {
-                int sum = (price.getPrice()*td.getAdultTicketCount())+
-                        (int)(price.getPrice()*td.getSeniorTicketCount()*0.4)+
-                        (int)(price.getPrice()*td.getTeenagerTicketCount()*0.5)+
-                        (int)(price.getPrice()*td.getChildrenTicketCount()*0.25);
+                int sum = (price.getPrice() * td.getAdultTicketCount()) +
+                        (int) (price.getPrice() * td.getSeniorTicketCount() * 0.4) +
+                        (int) (price.getPrice() * td.getTeenagerTicketCount() * 0.5) +
+                        (int) (price.getPrice() * td.getChildrenTicketCount() * 0.25);
                 System.out.println("============= 티켓금액은 " + price.getPrice() + "입니다. 결제를 계속 진행해주세요. =============");
                 pay.paymentMethod(sum);
                 break;
